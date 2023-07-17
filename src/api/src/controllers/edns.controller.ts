008f607ce67232abb4b8106bea18e36cd01b975f
shingseto <shing.seto@one2.cloud>
2023-07-17 17:31:06 +0800

    add queryHandler, fix queryEdnsAddress bug

--- src/api/src/controllers/edns.controller.ts
+++ src/api/src/controllers/edns.controller.ts
@@ -3,6 +3,48 @@ import { EdnsService } from "../services/edns.service";
 import { RedisService } from "../services/redis.service";
 
 import { EdnsV1FromContractService } from "../services/edns-v1.service";
+import { IOptions } from "../interfaces/IOptions.interface";
+import { InvalidQueryError } from "../errors/invalid-query.error";
+import { Net, Network } from "../network-config";
+
+function queryHandler(req: Request): IOptions {
+  let options: IOptions = {};
+  // Handle onChain
+  if (req.query.redis === undefined) {
+    options.onChain = undefined;
+  } else if (req.query.redis === "true" || req.query.redis === "false") {
+    options.onChain = (req.query.redis === 'false')
+  } else {
+    throw new InvalidQueryError({redis: req.query.redis})
+  }
+  // Handle version
+  if (req.query.version === undefined) {
+    options.version = undefined;
+  } else if (req.query.version === "v1" || req.query.version === "v2") {
+    options.version = req.query.version
+  } else {
+    throw new InvalidQueryError({version: req.query.version})
+  }
+  // Handle net
+  if (req.query.net === undefined) {
+    options.net = undefined;
+  } else if (req.query.net === "MAINNET" || req.query.net === "TESTNET") {
+    options.net = Net[req.query.net]
+  } else {
+    throw new InvalidQueryError({net: req.query.net})
+  }
+  // Handle chainId
+  const validChainId = Object.values(Network).filter(value => typeof value === 'number');
+  if (req.query.chainId === undefined) {
+    options.chainId = undefined;
+  } else if (typeof req.query.chainId === "number" && validChainId.includes(parseInt(req.query.chainId))) {
+    options.chainId = parseInt(req.query.chainId)
+  } else {
+    throw new InvalidQueryError({chainId: req.query.chainId})
+  }
+  console.log(options)
+  return options
+}
 
 export default class EdnsController {
   // public static async queryEdnsAddress(req: Request, res: Response, next: NextFunction): Promise<void> {
@@ -14,13 +56,13 @@ export default class EdnsController {
     let output;
     try {
       const { fqdn } = req.params;
+      const options = queryHandler(req)
       res.set("Function", "GetAddressUsingFqdn");
+      res.set("On-Chain", String(options.onChain === undefined ? true : options.onChain));
       const ednsService = new EdnsService();
-      res.set("On-Chain", `${req.query.redis}`);
-      output = await ednsService.getAddressRecord(`${fqdn}`, { onChain: Boolean(req.query.redis) });
+      output = await ednsService.getAddressRecord(`${fqdn}`, options);
       res.locals.result = output?.address;
       next();
-      // console.log(result)
     } catch (error) {
       res.locals.result = output?.address;
       next(error);
