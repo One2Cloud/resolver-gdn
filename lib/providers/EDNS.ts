4a6b73c6a8602e5d2a1022bef56d567e141b4a66
Cha0173 <jacob35723763@gmail.com>
2023-07-20 17:30:59 +0800

    add role

--- lib/providers/EDNS.ts
+++ lib/providers/EDNS.ts
@@ -19,7 +19,6 @@ export interface ConstructProps {
     ecs: ecra.DockerImageAsset;
     lambda: ecra.DockerImageAsset;
   };
-  sqs: sqs.Queue;
   secret: secretmanager.Secret;
 }
 
--- lib/resolver-gdn-stack.ts
+++ lib/resolver-gdn-stack.ts
@@ -10,55 +10,57 @@ import { GlobalApi } from "./providers/GlobalAPI";
 import * as route53 from "aws-cdk-lib/aws-route53";
 
 export class ResolverGdnStack extends cdk.Stack {
-	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-		super(scope, id, props);
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
 
-		const hostedzone = route53.PublicHostedZone.fromLookup(this, "HostedZone", { domainName: "resolver.gdn" });
+    const hostedzone = route53.PublicHostedZone.fromLookup(this, "HostedZone", {
+      domainName: "resolver.gdn",
+    });
 
-		const secret = new secretsmanager.Secret(this, "Secret");
+    const secret = new secretsmanager.Secret(this, "Secret");
 
-		const vpc = new ec2.Vpc(this, "Vpc", {
-			natGateways: 0,
-			subnetConfiguration: [
-				{
-					subnetType: ec2.SubnetType.PUBLIC,
-					name: "Public",
-				},
-			],
-		});
+    const vpc = new ec2.Vpc(this, "Vpc", {
+      natGateways: 0,
+      subnetConfiguration: [
+        {
+          subnetType: ec2.SubnetType.PUBLIC,
+          name: "Public",
+        },
+      ],
+    });
 
-		const cluster = new ecs.Cluster(this, "Cluster", {
-			vpc,
-			containerInsights: true,
-			enableFargateCapacityProviders: true,
-		});
+    const cluster = new ecs.Cluster(this, "Cluster", {
+      vpc,
+      containerInsights: true,
+      enableFargateCapacityProviders: true,
+    });
 
-		const ecsImages = new ecra.DockerImageAsset(this, "EcsImage", {
-			directory: path.join(process.cwd(), "src/listener"),
-			platform: ecra.Platform.LINUX_AMD64,
-			file: "docker/Dockerfile",
-		});
+    const ecsImages = new ecra.DockerImageAsset(this, "EcsImage", {
+      directory: path.join(process.cwd(), "src/listener"),
+      platform: ecra.Platform.LINUX_AMD64,
+      file: "docker/Dockerfile",
+    });
 
-		const lambdaImage = new ecra.DockerImageAsset(this, "LambdaImage", {
-			directory: path.join(process.cwd(), "src/listener"),
-			platform: ecra.Platform.LINUX_AMD64,
-			file: "docker/Dockerfile.lambda",
-			buildArgs: {
-				AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID!,
-				AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY!,
-				AWS_SESSION_TOKEN: process.env.AWS_SESSION_TOKEN!,
-			},
-		});
+    const lambdaImage = new ecra.DockerImageAsset(this, "LambdaImage", {
+      directory: path.join(process.cwd(), "src/listener"),
+      platform: ecra.Platform.LINUX_AMD64,
+      file: "docker/Dockerfile.lambda",
+      buildArgs: {
+        AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID!,
+        AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY!,
+        AWS_SESSION_TOKEN: process.env.AWS_SESSION_TOKEN!,
+      },
+    });
 
-		new EDNS(this, "EDNS", {
-			images: {
-				lambda: lambdaImage,
-				ecs: ecsImages,
-			},
-			secret,
-			cluster,
-		});
+    new EDNS(this, "EDNS", {
+      images: {
+        lambda: lambdaImage,
+        ecs: ecsImages,
+      },
+      secret,
+      cluster,
+    });
 
-		new GlobalApi(this, "GlobalApi", { hostedzone });
-	}
+    new GlobalApi(this, "GlobalApi", { hostedzone });
+  }
 }
