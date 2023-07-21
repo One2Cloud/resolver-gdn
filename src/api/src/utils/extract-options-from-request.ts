10e9e34051c69a1f3069c03fc4408a639d043a0a
Cha0173 <jacob35723763@gmail.com>
2023-07-21 17:59:21 +0800

    update put event

--- src/api/src/utils/extract-options-from-request.ts
+++ src/api/src/utils/extract-options-from-request.ts
@@ -4,9 +4,13 @@ import { Net } from "../network-config";
 
 export const extract = (req: Request): IOptions => {
   const options: IOptions = {};
+  console.log({ query: req.query });
+
   options.onchain = req.query.onchain === "true";
   options.chainId = req.query.chain_id ? parseInt(req.query.chain_id as string, 10) : undefined;
   options.net = req.query.net ? <Net>req.query.net : undefined;
   options.version = req.query.version ? (req.query.version as string) : undefined;
+  console.log("options: ", options);
+
   return options;
 };
--- src/listener/src/utils/put-event.ts
+++ src/listener/src/utils/put-event.ts
@@ -3,19 +3,33 @@ import config from "../config";
 import { EventType } from "../constants/event-type.constant";
 import { DomainProvider } from "../constants/domain-provider.constant";
 import { createLogger } from "./create-logger";
+import { buffer } from "stream/consumers";
 
 const logger = createLogger({ service: "put_sqs_event" });
 
-export const putEvent = async (chainId: number, provider: DomainProvider, fqdn: string, type: EventType, data: any) => {
-	const sqs = new SQS({ region: process.env.AWS_REGION });
-	logger.debug(`Putting event to SQS: ${fqdn}`);
-	try {
-		await sqs.sendMessage({
-			QueueUrl: config.sqs.handler.url,
-			MessageDeduplicationId: `${provider}:${fqdn}:${type}`,
-			MessageBody: JSON.stringify({ type, data, chainId, mainnet: process.env.MAINNET == "1" }),
-		});
-	} catch (err) {
-		console.error(err);
-	}
+export const putEvent = async (
+  chainId: number,
+  provider: DomainProvider,
+  fqdn: string,
+  type: EventType,
+  data: any
+) => {
+  const sqs = new SQS({ region: process.env.AWS_REGION });
+  logger.debug(`Putting event to SQS: ${fqdn}`);
+  try {
+    await sqs.sendMessage({
+      QueueUrl: config.sqs.handler.url,
+      MessageDeduplicationId: Buffer.from(
+        `${provider}:${fqdn}:${type}`
+      ).toString("hex"),
+      MessageBody: JSON.stringify({
+        type,
+        data,
+        chainId,
+        mainnet: process.env.MAINNET == "1",
+      }),
+    });
+  } catch (err) {
+    console.error(err);
+  }
 };
