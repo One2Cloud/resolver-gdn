ceab73fe92af91457b7b41477971338f16df78c2
Cha0173 <jacob35723763@gmail.com>
2023-07-24 16:23:48 +0800

    test put event

--- src/listener/src/utils/put-event.ts
+++ src/listener/src/utils/put-event.ts
@@ -3,7 +3,6 @@ import config from "../config";
 import { EventType } from "../constants/event-type.constant";
 import { DomainProvider } from "../constants/domain-provider.constant";
 import { createLogger } from "./create-logger";
-import { buffer } from "stream/consumers";
 
 const logger = createLogger({ service: "put_sqs_event" });
 
@@ -17,16 +16,16 @@ export const putEvent = async (
   const sqs = new SQS({ region: process.env.AWS_REGION });
   logger.debug(`Putting event to SQS: ${fqdn}`);
   logger.debug(
-    `Event: ${typeof Buffer.from(`${provider}:${fqdn}:${type}`).toString(
-      "hex"
-    )}`
+    `MessageDeduplicationId Length: ${
+      Buffer.from(`${provider}:${fqdn}:${type}`).toString("hex").length
+    }`
   );
   try {
     await sqs.sendMessage({
       QueueUrl: config.sqs.handler.url,
-      MessageDeduplicationId: `${Buffer.from(
+      MessageDeduplicationId: Buffer.from(
         `${provider}:${fqdn}:${type}`
-      ).toString("hex")}`,
+      ).toString("hex"),
       MessageBody: JSON.stringify({
         type,
         data,
