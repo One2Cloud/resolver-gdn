02794649d5de4d0f85455def5c536c23f23a5577
Cha0173 <jacob35723763@gmail.com>
2023-07-26 18:51:14 +0800

    add log to index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,10 @@
 require("source-map-support/register");
 import * as dotenv from "dotenv";
+import path from "path";
 import serverless from "@vendia/serverless-express";
 import app from "./src/app";
-dotenv.config({ path: ".env.runtime" });
+console.log("path: ", path.join(__dirname, ".env.runtime"));
+console.log("process_cwd: ", process.cwd());
+
+dotenv.config({ path: path.join(__dirname, ".env.runtime") });
 export const handler = serverless({ app });
