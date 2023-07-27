5db95a209d0b3d0f65601b28cebfcf3a0d6c0e07
Cha0173 <jacob35723763@gmail.com>
2023-07-27 18:03:05 +0800

    fix path

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,6 +3,14 @@ import * as dotenv from "dotenv";
 import path from "path";
 import serverless from "@vendia/serverless-express";
 import app from "./src/app";
+import fs from "fs";
+
+const envirament = fs.readFile(path.join(__dirname, ".env.runtime"), (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
+console.log(envirament);
+
 console.log("path: ", path.join(__dirname, ".env.runtime"));
 console.log("process_cwd: ", process.cwd());
 
