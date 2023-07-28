39a90bf10db4dea4bf325e2910f527e0a0ddc83f
Cha0173 <jacob35723763@gmail.com>
2023-07-28 16:41:47 +0800

    fix: env can not be read

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,14 +5,19 @@ import serverless from "@vendia/serverless-express";
 import app from "./src/app";
 import fs from "fs";
 
-const envirament = fs.readFile(path.join(__dirname, ".env.runtime"), (err, data) => {
-  if (err) throw err;
-  console.log(data.toString());
-});
-console.log(envirament);
+const environment = fs.readFileSync(path.join(__dirname, ".env.runtime"), "utf8");
 
-console.log("path: ", path.join(__dirname, ".env.runtime"));
-console.log("process_cwd: ", process.cwd());
-dotenv.config({ path: path.resolve(__dirname, ".env.runtime") });
-dotenv.config({ path: path.join(process.cwd(), ".env.runtime") });
+const lines = environment.split("\n").map((l) => l.split("="));
+
+for (const label of lines) {
+  const [key, value] = label;
+  process.env[key] = value;
+}
+
+console.log(process.env);
+
+// console.log("path: ", path.join(__dirname, ".env.runtime"));
+// console.log("process_cwd: ", process.cwd());
+// dotenv.config({ path: path.resolve(__dirname, ".env.runtime") });
+// dotenv.config({ path: path.resolve(process.cwd(), ".env.runtime") });
 export const handler = serverless({ app });
