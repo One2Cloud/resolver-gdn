9ae6f1e2454b1f28a2a3fd6036cc62c05154d003
Mike Ng <iam@mikeng.io>
2023-09-04 16:37:42 +0800

    Updated valid FQDN regex

--- src/utils/is-valid-fqdn.ts
+++ src/utils/is-valid-fqdn.ts
@@ -2,5 +2,5 @@ import fs from "fs";
 import path from "path";
 
 const EMOJI_REGEX = fs.readFileSync(path.join(process.cwd(), "static/domain-emoji-regex.txt"), "utf8");
-const FQDN_REGEX = new RegExp(`(?=^.{1,253}$)(^(((?!-)([a-z0-9-_]|${EMOJI_REGEX}){1,128}(?<!-))|((?!-)([a-z0-9-]|${EMOJI_REGEX}){1,128}(?<!-)\.)+[a-z0-9]{2,63})$)`, "u");
+const FQDN_REGEX = new RegExp(`(?=^.{1,253}$)(^(((?!-)([a-z0-9-_]|${EMOJI_REGEX}){1,128}(?<!-))|((?!-)([a-z0-9-]|${EMOJI_REGEX}){1,128}(?<!-)\.)+[a-z0-9]-_{2,63})$)`, "u");
 export const isValidFqdn = (fqdn: string): boolean => FQDN_REGEX.test(fqdn);
