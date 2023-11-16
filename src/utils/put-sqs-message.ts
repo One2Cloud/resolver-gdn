c998fbca792d127a18d8deba277e6da44abe9f92
JohnKHW <wongkahingerv@gmail.com>
2023-11-16 17:57:11 +0800

    style: eslint

--- src/utils/put-sqs-message.ts
+++ src/utils/put-sqs-message.ts
@@ -5,38 +5,40 @@ import { Net } from "../network-config";
 import { getConfig } from "../config";
 
 export interface IPutSqsMessageInput {
-	net?: Net;
-	eventType: EdnsEventType;
-	provider: DomainProvider;
-	chainId?: number;
-	fqdn: string;
-	hash?: string;
-	data: any;
+  net?: Net;
+  eventType: EdnsEventType;
+  provider: DomainProvider;
+  chainId?: number;
+  fqdn: string;
+  hash?: string;
+  data: any;
 }
 
 export const putSqsMessage = async (input: IPutSqsMessageInput) => {
-	if (!input.net) {
-		input.net = Net.MAINNET;
-	}
-	const config = getConfig();
-	const sqs = new SQS({ region: "us-east-1" });
-	console.debug(`Putting event to SQS: ${input.fqdn}`);
-	console.debug(`MessageDeduplication: ${`${input.provider}:${input.fqdn}:${input.eventType}`}`);
-	let queueUrl: string | undefined = undefined;
-	if (input.provider === DomainProvider.EDNS) {
-		queueUrl = config.edns.sqs.queue.url;
-	} else {
-		throw new Error(`Unsupported domain provider: ${input.provider}`);
-	}
-	try {
-		await sqs.sendMessage({
-			QueueUrl: queueUrl,
-			MessageBody: JSON.stringify({
-				...input,
-				mainnet: input.net === Net.MAINNET,
-			}),
-		});
-	} catch (err) {
-		console.error(err);
-	}
+  if (!input.net) {
+    input.net = Net.MAINNET;
+  }
+  const config = getConfig();
+  const sqs = new SQS({ region: "us-east-1" });
+  console.debug(`Putting event to SQS: ${input.fqdn}`);
+  console.debug(
+    `MessageDeduplication: ${`${input.provider}:${input.fqdn}:${input.eventType}`}`
+  );
+  let queueUrl: string | undefined = undefined;
+  if (input.provider === DomainProvider.EDNS) {
+    queueUrl = config.edns.sqs.queue.url;
+  } else {
+    throw new Error(`Unsupported domain provider: ${input.provider}`);
+  }
+  try {
+    await sqs.sendMessage({
+      QueueUrl: queueUrl,
+      MessageBody: JSON.stringify({
+        ...input,
+        mainnet: input.net === Net.MAINNET,
+      }),
+    });
+  } catch (err) {
+    console.error(err);
+  }
 };
