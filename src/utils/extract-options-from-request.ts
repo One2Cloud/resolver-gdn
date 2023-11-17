0fa92f9be0ba9986e97ec3aada7c9addb3041d64
JohnKHW <wongkahingerv@gmail.com>
2023-11-17 15:36:12 +0800

    fix: cannot classify onchain type to bool

--- src/utils/extract-options-from-request.ts
+++ src/utils/extract-options-from-request.ts
@@ -3,14 +3,21 @@ import { IOptions } from "../interfaces/IOptions.interface";
 import { Net } from "../network-config";
 
 export const extract = (req: Request): IOptions => {
-  const options: IOptions = {};
-  options.onchain = req.query.onchain === "true";
-  options.chainId = req.query.chain_id
-    ? parseInt(req.query.chain_id as string, 10)
-    : undefined;
-  options.net = req.query.net ? <Net>req.query.net : undefined;
-  options.version = req.query.version
-    ? (req.query.version as string)
-    : undefined;
-  return options;
+  const {
+    onchain, // Default value for onchain
+    chain_id,
+    version = "v2", // Default value for version
+    net = Net.MAINNET, // Default value for net
+  } = req.query;
+
+  // Convert chain_id to a number if it's not undefined
+  const chainId = chain_id !== undefined ? Number(chain_id) : undefined;
+
+  // Return an object of type IOptions
+  return {
+    chainId,
+    net: net as Net,
+    version: version as string,
+    onchain: onchain === "true",
+  };
 };
