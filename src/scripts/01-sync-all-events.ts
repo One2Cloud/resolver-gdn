e490ba34e29df63969f8a27d92031724ebb47022
Mike Ng <iam@mikeng.io>
2024-01-17 15:11:09 +0800

    Added try-catch to `sync-all-event` to prevent interupt

--- src/scripts/01-sync-all-events.ts
+++ src/scripts/01-sync-all-events.ts
@@ -24,7 +24,7 @@ const BATCH = 200;
 
 async function main() {
   await setEnvironmentVariable();
-  
+
   const NetworkConfig = getNetworkConfig();
 
   const client = new Lambda({ region: "us-east-1" });
@@ -36,34 +36,38 @@ async function main() {
   const TO = await provider.getBlockNumber();
 
   for (let _from = METADATA["Registrar"].block; _from <= TO; _from += BATCH) {
-    const eventTypes = _.values(EdnsEventType);
-    for (const eventType of eventTypes) {
-      let _to = _from + BATCH;
+    try {
+      const eventTypes = _.values(EdnsEventType);
+      for (const eventType of eventTypes) {
+        let _to = _from + BATCH;
 
-      const latest = await provider.getBlockNumber();
-      console.log(`[${latest}] Invoking ${eventType} for ${_from} - ${_to}.`);
+        const latest = await provider.getBlockNumber();
+        console.log(`[${latest}] Invoking ${eventType} for ${_from} - ${_to}.`);
 
-      if (_to > TO) {
-        await client.invokeAsync({
-          FunctionName: "EDNS-Sync-Event",
-          InvokeArgs: JSON.stringify({
-            chainId: networkConfig.chainId,
-            from: _from,
-            to: await provider.getBlockNumber(),
-            eventType,
-          }),
-        });
-      } else {
-        await client.invokeAsync({
-          FunctionName: "EDNS-Sync-Event",
-          InvokeArgs: JSON.stringify({
-            chainId: networkConfig.chainId,
-            from: _from,
-            to: _to,
-            eventType,
-          }),
-        });
+        if (_to > TO) {
+          await client.invokeAsync({
+            FunctionName: "EDNS-Sync-Event",
+            InvokeArgs: JSON.stringify({
+              chainId: networkConfig.chainId,
+              from: _from,
+              to: await provider.getBlockNumber(),
+              eventType,
+            }),
+          });
+        } else {
+          await client.invokeAsync({
+            FunctionName: "EDNS-Sync-Event",
+            InvokeArgs: JSON.stringify({
+              chainId: networkConfig.chainId,
+              from: _from,
+              to: _to,
+              eventType,
+            }),
+          });
+        }
       }
+    } catch (error) {
+      console.error(error);
     }
   }
 }
