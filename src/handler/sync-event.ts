372d87a653a6d23f76c99703c95694f975125bd7
Mike Ng <iam@mikeng.io>
2024-01-17 13:57:19 +0800

    Added `setEnvironmentVariable` in `sync-event`

--- src/handler/sync-event.ts
+++ src/handler/sync-event.ts
@@ -1,6 +1,7 @@
 import { Handler } from "aws-lambda";
 import { Net } from "../network-config";
 import { sync } from "../app/listener/main";
+import { setEnvironmentVariable } from "../utils/set-environment-variable";
 
 interface Input {
   chainId: number;
@@ -11,5 +12,6 @@ interface Input {
 }
 
 export const index: Handler<Input> = async (input) => {
+  await setEnvironmentVariable();
   await sync({ ...input });
 };
