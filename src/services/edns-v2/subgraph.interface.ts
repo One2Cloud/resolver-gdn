097fce19468fc31c53d0041deec2541c7bf25a01
Alex-Wong-HK <alexwg2015@gmail.com>
2024-04-22 18:15:58 +0800

    add two to do

---
+++ src/services/edns-v2/subgraph.interface.ts
@@ -0,0 +1,35 @@
+export enum IDomainType {
+    UNIVERSAL = 'universal',
+    CLASSICAL = 'classical',
+    OMNI = 'omni'
+}
+
+export interface IGetWalletInfoOutput {
+    address: string;
+    resversedDomain: string | null;
+    domains: {
+        fqdn: string;
+        chainId: number;
+        type: IDomainType;
+        tokenId: string;
+        expiryDate: number;
+    }
+}
+
+export interface IDomainDetailsOutput {
+    chainId: number;
+    type: IDomainType;
+    owner: string; //Address
+    tokenId: string;
+    expiryDate: number;
+    records: {
+        text: string,
+        address: string,
+        typedText: {
+            [key: string]: string
+        },
+        typedAddress: {
+            [key: string]: string
+        }
+    }
+}
