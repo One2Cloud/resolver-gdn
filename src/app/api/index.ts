634881b4b00ee68ead9abafc7655533fdccb8ef2
Mike Ng <iam@mikeng.io>
2024-04-30 01:36:08 +0800

    Updated to setEnvironmentVariable in App Runner API

--- src/app/api/index.ts
+++ src/app/api/index.ts
@@ -1,8 +1,10 @@
 require("source-map-support/register");
 
+import { setEnvironmentVariable } from "../../utils/set-environment-variable";
 import app from "./app";
 
 async function main() {
+  await setEnvironmentVariable();
   app.listen(process.env.PORT || 8080, () => {
     console.log(`Server listening on port ${process.env.PORT || 8080}`);
   });
--- src/utils/set-environment-variable.ts
+++ src/utils/set-environment-variable.ts
@@ -1,12 +1,16 @@
 import { getSecret } from "./get-secret";
 
 export const setEnvironmentVariable = async () => {
-  if (process.env.SECRET_ARN) {
-    const vars: { [key: string]: string } = JSON.parse(await getSecret(process.env.SECRET_ARN));
+  if (process.env.GLOBAL_SECRET_VALUE) {
+    const vars: { [key: string]: string } = JSON.parse(process.env.GLOBAL_SECRET_VALUE);
     for (const _var in vars) {
       process.env[_var] = vars[_var];
     }
-  } else {
-    console.warn("SECRET_ARN is not set");
+  }
+  if (process.env.GLOBAL_SECRET_ARN) {
+    const vars: { [key: string]: string } = JSON.parse(await getSecret(process.env.GLOBAL_SECRET_ARN));
+    for (const _var in vars) {
+      process.env[_var] = vars[_var];
+    }
   }
 };
